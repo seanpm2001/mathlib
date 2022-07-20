ee023c5745a145909be0908a7d3100ed024aa0f4
Yury G. Kudryashov <urkud@urkud.name>
2022-07-20 08:23:27 -0400

    Fix import

--- src/data/set/countable.lean
+++ src/data/set/countable.lean
@@ -1,9 +1,10 @@
 /-
 Copyright (c) 2017 Johannes Hölzl. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johannes Hölzl
+Authors: Johannes Hölzl, Yury Kudryashov
 -/
 import data.set.finite
+import data.countable.basic
 import logic.equiv.list
 
 /-!
